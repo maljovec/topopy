9f22cd054cb93d55267be9f382d8c591e04fb3ae
Dan Maljovec <maljovec002@gmail.com>
2018-04-07 08:15:02 -0600

    Adding test_to file and moving relevant code from test_msc to this file.

--- topopy/tests/test_msc.py
+++ topopy/tests/test_msc.py
@@ -168,87 +168,6 @@ class TestMSC(TestCase):
                              'The merge sequence does not match the ' +
                              'expected output.')
 
-    def test_get_names(self):
-        """ Test the ability for the code to generate dummy names
-            and also correctly use passed in names.
-        """
-        self.setup()
-
-        default_names = []
-        for d in range(self.msc.get_dimensionality()):
-            default_names.append('x%d' % d)
-        default_names.append('y')
-
-        test_names = self.msc.get_names()
-        for i in range(len(default_names)):
-            self.assertEqual(default_names[i], test_names[i],
-                             'The MorseSmaleComplex object should generate ' +
-                             'default value names for labeling purposes.')
-
-        custom_names = ['a', 'b', 'c']
-        self.msc.build(self.X, self.Y, names=custom_names)
-        test_names = self.msc.get_names()
-        for i in range(len(custom_names)):
-            self.assertEqual(custom_names[i], test_names[i],
-                             'The MorseSmaleComplex object should use any ' +
-                             'custom names passed in for labeling purposes.')
-
-    def test_get_normed_x(self):
-        """ Tests get_normed_x in several different contexts:
-                Single Element extraction
-                Single Column extraction
-                Single Row extraction
-                Multiple row extraction
-                Multiple column extraction
-                Full data extraction
-        """
-        self.setup()
-
-        for norm, X in self.norm_x.items():
-            msc = topopy.MorseSmaleComplex(normalization=norm)
-            msc.build(self.X, self.Y)
-
-            # Test single column extraction
-            for col in range(X.shape[1]):
-                column_values = msc.get_normed_x(cols=col)
-                np.testing.assert_array_equal(X[:, col], column_values,
-                                              'get_normed_x should be able ' +
-                                              'to access a full column of ' +
-                                              'the input data.')
-
-            # Test single row extraction
-            for row in range(X.shape[0]):
-                row_values = msc.get_normed_x(row).flatten()
-                np.testing.assert_array_equal(X[row, :], row_values,
-                                              'get_normed_x should be able ' +
-                                              'to access a full row of the ' +
-                                              'input data.')
-                # Test single element extraction
-                for col in range(X.shape[1]):
-                    self.assertEqual(X[row, col],
-                                     msc.get_normed_x(row, col),
-                                     'get_normed_x should be able to access ' +
-                                     'a single element of the input data.')
-
-            # Multiple row extraction
-            row_values = msc.get_normed_x(list(range(X.shape[0])), 0)
-            np.testing.assert_array_equal(X[:, 0], row_values,
-                                          'get_normed_x should be able to ' +
-                                          'access multiple rows of the ' +
-                                          'input data.')
-
-            # Multiple column extraction
-            col_values = msc.get_normed_x(0, list(range(X.shape[1]))).flatten()
-            np.testing.assert_array_equal(X[0, :], col_values,
-                                          'get_normed_x should be able to ' +
-                                          'access multiple columns of the ' +
-                                          'input data.')
-
-            # Full data extraction
-            np.testing.assert_array_equal(X, msc.get_normed_x(),
-                                          'get_normed_x should be able to ' +
-                                          'access the entire input data.')
-
     def test_get_weights(self):
         """ Function to test if default weights can be applied to the
             MorseSmaleComplex object. This feature should be evaluated
@@ -269,88 +188,6 @@ class TestMSC(TestCase):
                                       'User should be able to filter the ' +
                                       'rows retrieved from get_weights.')
 
-    def test_get_x(self):
-        """ Tests get_x in several different contexts:
-                Single Element extraction
-                Single Column extraction
-                Single Row extraction
-                Multiple row extraction
-                Multiple column extraction
-                Full data extraction
-        """
-        self.setup()
-
-        # Test single column extraction
-        for col in range(self.X.shape[1]):
-            column_values = self.msc.get_x(cols=col)
-            np.testing.assert_array_equal(self.X[:, col], column_values,
-                                          'get_x should be able to access a ' +
-                                          'full column of the input data.')
-
-        # Test single row extraction
-        for row in range(self.X.shape[0]):
-            row_values = self.msc.get_x(row).flatten()
-            np.testing.assert_array_equal(self.X[row, :], row_values,
-                                          'get_x should be able to access a ' +
-                                          'full row of the input data.')
-            # Test single element extraction
-            for col in range(self.X.shape[1]):
-                self.assertEqual(self.X[row, col], self.msc.get_x(row, col),
-                                 'get_x should be able to access a single ' +
-                                 'element of the input data.')
-
-        # Multiple row extraction
-        row_values = self.msc.get_x(list(range(self.X.shape[0])), 0)
-        np.testing.assert_array_equal(self.X[:, 0], row_values,
-                                      'get_x should be able to access ' +
-                                      'multiple rows of the input data.')
-
-        # Multiple column extraction
-        col_values = self.msc.get_x(0, list(range(self.X.shape[1]))).flatten()
-        np.testing.assert_array_equal(self.X[0, :], col_values,
-                                      'get_x should be able to access ' +
-                                      'multiple columns of the input data.')
-
-        # Full data extraction
-        np.testing.assert_array_equal(self.X, self.msc.get_x(),
-                                      'get_x should be able to access ' +
-                                      'the entire input data.')
-
-        # Empty query
-        np.testing.assert_array_equal([], self.msc.get_x([]),
-                                      'get_x should be able to access ' +
-                                      'return an empty array on null filter.')
-
-    def test_get_y(self):
-        """ Tests get_y in several different contexts:
-                Single Element extraction
-                Multiple row extraction
-                Full data extraction
-        """
-        self.setup()
-
-        for row in range(len(self.Y)):
-            # Test single element extraction
-            self.assertEqual(self.Y[row], self.msc.get_y(row),
-                             'get_y should be able to access a single ' +
-                             'element of the input data.')
-
-        # Multiple row extraction
-        row_values = self.msc.get_y(list(range(len(self.Y))))
-        np.testing.assert_array_equal(self.Y, row_values,
-                                      'get_y should be able to access ' +
-                                      'multiple rows of the input data.')
-
-        # Full data extraction
-        np.testing.assert_array_equal(self.Y, self.msc.get_y(),
-                                      'get_y should be able to access ' +
-                                      'the entire input data.')
-
-        # Empty query
-        np.testing.assert_array_equal([], self.msc.get_y([]),
-                                      'get_y should be able to access ' +
-                                      'return an empty array on null filter.')
-
     def test_load_data_and_build(self):
         """ Tests that loading the same test data from file yields an
             equivalent result
@@ -370,15 +207,6 @@ class TestMSC(TestCase):
         self.assertDictEqual(self.msc.base_partitions, msc.base_partitions,
                              'loading from file should produce the base ' +
                              'partitions.')
-
-    def test_neighbors(self):
-        """ Tests the ability to retrieve the neighbors of a given index
-        """
-        self.setup()
-        self.assertSetEqual({40, 1}, set(self.msc.get_neighbors(0)),
-                            'get_neighbors should return a list of integer ' +
-                            'indices indicating who is connected to the ' +
-                            'given index.')
 
     def test_persistence(self):
         """ Tests the getting and setting of different persistence
