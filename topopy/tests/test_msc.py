b768eb8573b0c37c4bc4996d6c3a971e2dde7389
Dan Maljovec <maljovec002@gmail.com>
2018-09-07 11:15:53 -0600

    * More robust testing of the Morse-Smale Complex that won't report as many false negatives * Renaming variable to test_object to be more agnostic of code in case any of this needs to be dropped in from other example test cases. * Leaving breadcrumbs for improving testing further. * Blacking test_msc file.

--- topopy/tests/test_msc.py
+++ topopy/tests/test_msc.py
@@ -17,6 +17,13 @@ class TestMSC(TestCase):
     Class for testing the Morse-Smale Complex
     """
 
+    def __init__(self, *args, **kwargs):
+        super(TestMSC, self).__init__(*args, **kwargs)
+
+        # Python 2.7 compatibility
+        if not hasattr(self, "assertCountEqual"):
+            self.assertCountEqual = self.assertItemsEqual
+
     def setup(self):
         """
         Setup function will create a fixed point set and parameter
@@ -37,8 +44,8 @@ class TestMSC(TestCase):
         # __init__
         # build
         # __set_data
-        self.msc = topopy.MorseSmaleComplex(debug=False, max_neighbors=10)
-        self.msc.build(self.X, self.Y)
+        self.test_object = topopy.MorseSmaleComplex(debug=False, max_neighbors=10)
+        self.test_object.build(self.X, self.Y)
 
     def test_debug(self):
         """
@@ -49,8 +56,8 @@ class TestMSC(TestCase):
         test_file = "msc_test_debug.txt"
         sys.stdout = open(test_file, "w")
 
-        self.msc = topopy.MorseSmaleComplex(debug=True, max_neighbors=10)
-        self.msc.build(self.X, self.Y)
+        self.test_object = topopy.MorseSmaleComplex(debug=True, max_neighbors=10)
+        self.test_object.build(self.X, self.Y)
 
         sys.stdout.close()
         lines = [
@@ -76,7 +83,7 @@ class TestMSC(TestCase):
         self.setup()
 
         self.assertEqual(
-            len(self.msc.base_partitions.keys()),
+            len(self.test_object.base_partitions.keys()),
             16,
             "The 2D Gerber test function should have 16 "
             "partitions at the base level.",
@@ -91,17 +98,17 @@ class TestMSC(TestCase):
 
         self.assertEqual(
             "minimum",
-            self.msc.get_classification(0),
+            self.test_object.get_classification(0),
             "get_classification has misidentified a local " "minimum",
         )
         self.assertEqual(
             "maximum",
-            self.msc.get_classification(429),
+            self.test_object.get_classification(429),
             "get_classification has misidentified a local " "maximum",
         )
         self.assertEqual(
             "regular",
-            self.msc.get_classification(22),
+            self.test_object.get_classification(22),
             "get_classification has misidentified a regular " "point",
         )
 
@@ -131,15 +138,15 @@ class TestMSC(TestCase):
                 (984, 1189),
                 (39, 429),
             },
-            set(self.msc.get_current_labels()),
+            set(self.test_object.get_current_labels()),
             "The base "
             "partition labels returned from " + "get_current_labels does not match.",
         )
 
-        self.msc.set_persistence(self.msc.persistences[-1])
+        self.test_object.set_persistence(self.test_object.persistences[-1])
         self.assertSetEqual(
             {(1599, 410)},
-            set(self.msc.get_current_labels()),
+            set(self.test_object.get_current_labels()),
             "The base "
             "partition labels returned from " + "get_current_labels does not match.",
         )
@@ -153,18 +160,18 @@ class TestMSC(TestCase):
 
         self.assertEqual(
             (0, 410),
-            self.msc.get_label(0),
+            self.test_object.get_label(0),
             "The label of a " "single index should be retrievable.",
         )
 
         self.assertEqual(
             len(self.Y),
-            len(self.msc.get_label()),
+            len(self.test_object.get_label()),
             "The label " "of the entire dataset should be retrievable.",
         )
 
         gold_labels = np.array([[0, 410], [0, 410]])
-        test_labels = np.array(self.msc.get_label([0, 1]).flatten().tolist())
+        test_labels = np.array(self.test_object.get_label([0, 1]).flatten().tolist())
         np.testing.assert_array_equal(
             gold_labels,
             test_labels,
@@ -173,7 +180,7 @@ class TestMSC(TestCase):
 
         self.assertEqual(
             len(self.Y),
-            len(self.msc.get_label()),
+            len(self.test_object.get_label()),
             "Requesting "
             "labels without specifying an index should return a "
             "list of all labels",
@@ -181,7 +188,7 @@ class TestMSC(TestCase):
 
         self.assertEqual(
             [],
-            self.msc.get_label([]),
+            self.test_object.get_label([]),
             "Requesting labels for " "an empty query should return an empty list",
         )
 
@@ -192,23 +199,21 @@ class TestMSC(TestCase):
         """
         self.setup()
 
+        with open("msc_gold.json", "r") as data_file:
+            gold_json = data_file.read()
+            gold_json = json.loads(gold_json)
+
+        merge_sequence = {}
+        for item in gold_json["Hierarchy"]:
+            merge_sequence[item["Dying"]] = (
+                item["Persistence"],
+                item["Surviving"],
+                item["Saddle"],
+            )
+
         self.assertDictEqual(
-            {
-                0: (0.500192, 1560, 400),
-                960: (0.117402, 1560, 1160),
-                1189: (0.117402, 429, 989),
-                39: (0.500192, 1599, 439),
-                999: (0.117402, 1599, 1199),
-                429: (0.256168, 410, 424),
-                1584: (0.256168, 1599, 1589),
-                1560: (0.500192, 1599, 1570),
-                1170: (0.117402, 410, 970),
-                24: (0.256168, 39, 29),
-                984: (0.117402, 1584, 1184),
-                410: (1.99362, 410, 410),
-                1599: (1.99362, 1599, 1599),
-            },
-            self.msc.get_merge_sequence(),
+            merge_sequence,
+            self.test_object.get_merge_sequence(),
             "The merge sequence does not match the " + "expected output.",
         )
 
@@ -224,11 +229,13 @@ class TestMSC(TestCase):
 
         np.testing.assert_array_equal(
             equal_weights,
-            self.msc.get_weights(),
+            self.test_object.get_weights(),
             "The default weights should be 1 for " "every row in the input.",
         )
 
-        test_weights = self.msc.get_weights(list(range(len(self.msc.w))))
+        test_weights = self.test_object.get_weights(
+            list(range(len(self.test_object.w)))
+        )
 
         np.testing.assert_array_equal(
             equal_weights,
@@ -236,7 +243,7 @@ class TestMSC(TestCase):
             "User should be able to filter the " "rows retrieved from get_weights.",
         )
 
-        test_weights = self.msc.get_weights([])
+        test_weights = self.test_object.get_weights([])
 
         np.testing.assert_array_equal(
             [], test_weights, "An empty query should return empty" "results."
@@ -249,7 +256,7 @@ class TestMSC(TestCase):
         """
         self.setup()
 
-        msc = topopy.MorseSmaleComplex()
+        local_object = topopy.MorseSmaleComplex()
 
         np.savetxt(
             "test_file.csv",
@@ -257,29 +264,24 @@ class TestMSC(TestCase):
             delimiter=",",
             header="x0,x1,y",
         )
-        msc.load_data_and_build("test_file.csv", delimiter=",")
+        local_object.load_data_and_build("test_file.csv", delimiter=",")
         os.remove("test_file.csv")
 
         self.assertDictEqual(
-            self.msc.merge_sequence,
-            msc.merge_sequence,
+            self.test_object.merge_sequence,
+            local_object.merge_sequence,
             "loading from file should produce the same hierarchy",
         )
-        # self.assertDictEqual(
-        #     self.msc.base_partitions,
-        #     msc.base_partitions,
-        #     "loading from file should produce the base partitions.",
-        # )
         self.assertSetEqual(
-            set(self.msc.base_partitions.keys()),
-            set(msc.base_partitions.keys()),
+            set(self.test_object.base_partitions.keys()),
+            set(local_object.base_partitions.keys()),
             "loading from file should produce the base partitions.",
         )
 
-        for key in self.msc.base_partitions.keys():
+        for key in self.test_object.base_partitions.keys():
             self.assertListEqual(
-                self.msc.base_partitions[key].tolist(),
-                msc.base_partitions[key].tolist(),
+                self.test_object.base_partitions[key].tolist(),
+                local_object.base_partitions[key].tolist(),
                 "loading from file should produce the base partitions.",
             )
 
@@ -290,10 +292,10 @@ class TestMSC(TestCase):
         decreases correctly as persistence is increased
         """
         self.setup()
-        self.msc.set_persistence(self.msc.persistences[1])
+        self.test_object.set_persistence(self.test_object.persistences[1])
         self.assertEqual(
-            self.msc.persistences[1],
-            self.msc.get_persistence(),
+            self.test_object.persistences[1],
+            self.test_object.get_persistence(),
             "Users should be able to get and set the " + "persistence.",
         )
 
@@ -307,12 +309,18 @@ class TestMSC(TestCase):
             gold_json = data_file.read()
             gold_json = json.loads(gold_json)
 
-        test_json = json.loads(self.msc.to_json())
+        test_json = json.loads(self.test_object.to_json())
 
-        self.assertDictEqual(
-            gold_json,
-            test_json,
-            "The hierarchy printed " + "differs from what is expected.",
+        self.assertCountEqual(
+            gold_json["Hierarchy"],
+            test_json["Hierarchy"],
+            "The hierarchy printed differs from what is expected.",
+        )
+
+        self.assertListEqual(
+            gold_json["Partitions"],
+            test_json["Partitions"],
+            "The base partitions do not match",
         )
 
     def test_reset(self):
@@ -320,49 +328,57 @@ class TestMSC(TestCase):
         Tests resetting of internal storage of the msc object
         """
         self.setup()
-        self.msc.reset()
+        self.test_object.reset()
 
         self.assertListEqual(
             [],
-            self.msc.persistences,
+            self.test_object.persistences,
             "reset should clear all internal storage of the msc.",
         )
         self.assertDictEqual(
             {},
-            self.msc.base_partitions,
+            self.test_object.base_partitions,
             "reset should clear all internal storage of the msc.",
         )
         self.assertEqual(
             0,
-            self.msc.persistence,
+            self.test_object.persistence,
             "reset should clear all internal storage of the msc.",
         )
         self.assertDictEqual(
             {},
-            self.msc.merge_sequence,
+            self.test_object.merge_sequence,
             "reset should clear all internal storage of the msc.",
         )
         self.assertListEqual(
             [],
-            self.msc.min_indices,
+            self.test_object.min_indices,
             "reset should clear all internal storage of the msc.",
         )
         self.assertListEqual(
             [],
-            self.msc.max_indices,
+            self.test_object.max_indices,
             "reset should clear all internal storage of the msc.",
         )
         self.assertEqual(
-            [], self.msc.X, "reset should clear all internal storage of the msc."
+            [],
+            self.test_object.X,
+            "reset should clear all internal storage of the msc.",
         )
         self.assertEqual(
-            [], self.msc.Y, "reset should clear all internal storage of the msc."
+            [],
+            self.test_object.Y,
+            "reset should clear all internal storage of the msc.",
         )
         self.assertEqual(
-            [], self.msc.w, "reset should clear all internal storage of the msc."
+            [],
+            self.test_object.w,
+            "reset should clear all internal storage of the msc.",
         )
         self.assertEqual(
-            [], self.msc.Xnorm, "reset should clear all internal storage of the msc."
+            [],
+            self.test_object.Xnorm,
+            "reset should clear all internal storage of the msc.",
         )
 
     def test_save(self):
@@ -370,7 +386,7 @@ class TestMSC(TestCase):
         Testing the save feature correctly dumps to a json file.
         """
         self.setup()
-        self.msc.save("test.json")
+        self.test_object.save("test.json")
 
         with open("msc_gold.json", "r") as data_file:
             gold_json = data_file.read()
@@ -380,23 +396,37 @@ class TestMSC(TestCase):
             test_json = data_file.read()
             test_json = json.loads(test_json)
 
-        self.assertDictEqual(
-            gold_json,
-            test_json,
-            "save does not reproduce the same results as the gold custom json file.",
+        self.assertCountEqual(
+            gold_json["Hierarchy"],
+            test_json["Hierarchy"],
+            "The hierarchy differs from what is expected.",
         )
+
+        self.assertListEqual(
+            gold_json["Partitions"],
+            test_json["Partitions"],
+            "The base partitions do not match",
+        )
+
         os.remove("test.json")
 
-        self.msc.save()
+        self.test_object.save()
         with open("morse_smale_complex.json", "r") as data_file:
             test_json = data_file.read()
             test_json = json.loads(test_json)
 
-        self.assertDictEqual(
-            gold_json,
-            test_json,
-            "save does not reproduce the same results as the gold default json file.",
+        self.assertCountEqual(
+            gold_json["Hierarchy"],
+            test_json["Hierarchy"],
+            "The hierarchy differs from what is expected.",
         )
+
+        self.assertListEqual(
+            gold_json["Partitions"],
+            test_json["Partitions"],
+            "The base partitions do not match.",
+        )
+
         os.remove("morse_smale_complex.json")
 
     def test_shape_functions(self):
@@ -407,24 +437,24 @@ class TestMSC(TestCase):
 
         self.assertEqual(
             self.X.shape[1],
-            self.msc.get_dimensionality(),
+            self.test_object.get_dimensionality(),
             "get_dimensionality should return the number of " + "columns in X.",
         )
         self.assertEqual(
             self.X.shape[0],
-            self.msc.get_sample_size(),
+            self.test_object.get_sample_size(),
             "get_sample_size should return the number of " + "rows in X.",
         )
         self.assertEqual(
             121,
-            self.msc.get_sample_size((1599, 1189)),
+            self.test_object.get_sample_size((1599, 1189)),
             "get_sample_size should return the number of "
             + "rows in the specified partition.",
         )
 
     def test_get_partitions(self):
         self.setup()
-        partitions = self.msc.get_partitions()
+        partitions = self.test_object.get_partitions()
         self.assertEqual(
             16,
             len(partitions),
@@ -435,22 +465,26 @@ class TestMSC(TestCase):
             "(0) persistence level",
         )
 
-        partitions = self.msc.get_stable_manifolds(0)
+        partitions = self.test_object.get_stable_manifolds()
         self.assertEqual(
             4,
             len(partitions),
             "The number of stable manifolds " "should be 4 at the base level",
         )
+        # Assert ids
 
-        partitions = self.msc.get_unstable_manifolds(0)
+        partitions = self.test_object.get_unstable_manifolds()
         self.assertEqual(
             9,
             len(partitions),
             "The number of unstable " "manifolds should be 9 at the " "base level",
         )
+        # Assert ids
 
-        self.msc = topopy.MorseSmaleComplex()
-        partitions = self.msc.get_partitions()
+        # Get partitions, un/stable with requested persistence of 0.5
+
+        self.test_object = topopy.MorseSmaleComplex()
+        partitions = self.test_object.get_partitions()
         self.assertEqual(
             {},
             partitions,
