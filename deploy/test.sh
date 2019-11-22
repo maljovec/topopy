07121153ea06bf5a46140ef1d7724cc8b202b19b
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2019-11-22 01:01:36 -0700

    Removing whitespace from temporary variable

--- deploy/test.sh
+++ deploy/test.sh
@@ -4,8 +4,8 @@ set -x
 
 # Append the version number with this git commit hash
 GIT_HASH=$(git rev-parse --short HEAD)
-awk -v hash=$GIT_HASH '/^__version__ = \"/{ sub(/"$/,"-"hash"&") }1' topopy/__init__.py > tmp && mv tmp topopy/__init__.py
-TEMP_VERSION=$(grep  '__version__ = ' topopy/__init__.py | cut -d = -f 2)
+awk -v hash=$GIT_HASH '/^__version__ = \"/{ sub(/"$/,".dev"hash"&") }1' topopy/__init__.py > tmp && mv tmp topopy/__init__.py
+TEMP_VERSION=$(grep  '__version__ = ' topopy/__init__.py | cut -d = -f 2 | sed "s/\"//g" | sed 's/^[ \t]*//;s/[ \t]*$//')
 echo $TEMP_VERSION
 
 # Build the project
@@ -15,6 +15,6 @@ python setup.py sdist
 # Test the upload
 TWINE_USERNAME=__twine__
 TWINE_PASSWORD=$PYPI_TOKEN
-twine upload --repository-url https://test.pypi.org/legacy/ dist/topopy-$TEMP_VERSION.tar.gz
+twine upload --repository-url https://test.pypi.org/legacy/ dist/topopy-${TEMP_VERSION}.tar.gz
 pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple topopy==TEMP_VERSION
 python -c "import topopy"
